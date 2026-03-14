baf4b406f80b78570766cfee470f86dab8be6a80
VIETPK\vietp <vietpkh0502@gmail.com>
2026-03-14 21:18:03 +0700

    feat(order): add orders list and order detail view

--- HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/service/OrdersService.java
+++ HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/service/OrdersService.java
@@ -1,5 +1,13 @@
 package com.hsf.hsf302_ecom.service;
 
+import com.hsf.hsf302_ecom.entity.Orders;
+
+import java.util.List;
+
 public interface OrdersService {
+
+    List<Orders> getOrdersByUser(Long userId);
+
+    Orders getOrderDetail(Long orderId, Long userId);
 
 }
