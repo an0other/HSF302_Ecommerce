74d47259a1bf794efc60d2bac706802bafcaa644
Trần Quang Thành <iamjustan0other@gmail.com>
2026-03-26 22:53:11 +0700

    Fix img product

--- HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/repository/ProductImagesRepo.java
+++ HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/repository/ProductImagesRepo.java
@@ -2,9 +2,24 @@ package com.hsf.hsf302_ecom.repository;
 
 import com.hsf.hsf302_ecom.entity.ProductImages;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductImagesRepo extends JpaRepository<ProductImages, Long> {
 
+    List<ProductImages> findByProductIdOrderByIsPrimaryDescIdAsc(Long productId);
+
+    long countByProductId(Long productId);
+
+    Optional<ProductImages> findFirstByProductIdAndIsPrimaryTrue(Long productId);
+
+    @Modifying
+    @Query("UPDATE ProductImages pi SET pi.isPrimary = false WHERE pi.product.id = :productId")
+    void clearPrimaryForProduct(@Param("productId") Long productId);
 }
--- HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/service/ProductImagesService.java
+++ HSF302_Ecom/src/main/java/com/hsf/hsf302_ecom/service/ProductImagesService.java
@@ -1,5 +1,17 @@
 package com.hsf.hsf302_ecom.service;
 
+import com.hsf.hsf302_ecom.entity.ProductImages;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface ProductImagesService {
 
+    List<ProductImages> uploadImages(Long productId, List<MultipartFile> files);
+
+    void deleteImage(Long imageId, Long productId);
+
+    void setPrimary(Long imageId, Long productId);
+
+    List<ProductImages> getImages(Long productId);
 }
